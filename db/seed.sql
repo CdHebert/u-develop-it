0fe3ba93507ef838ab21a9633b847e6148d0d71b
CBear <chebert222@gmail.com>
2021-05-26 19:57:15 -0700

    joined both tables active

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,25 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
+
+DELETE FROM parties;
+DELETE FROM candidates;
+
+  INSERT INTO parties (name, description)
 VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+
+
+
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+VALUES
+  ('Sitama', 'Caped Baldy', 1, 99),
+  ('Genos', 'Cyborg', 1, 30),
+  ('Bang', 'SilverFang', 1, 45),
+  ('Garu', 'Monster', 2, 48),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 0),
+  ('Edward', 'Bellamy', 3, 0),
+  ('Montague', 'Summers', 3, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', NULL, 1);
+
